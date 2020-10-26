6abbffd27c642226ac6842871ff757cdbb1e7696
LeoWuxg <wxiangg163@163.com>
2020-10-26 23:41:30 +0800

    netty init

---
+++ bio/src/main/java/com/leo/netBasic/UseInet.java
@@ -0,0 +1,18 @@
+package com.leo.netBasic;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * @ClassName: UseInet
+ * @Description: InetAddress基本使用
+ * @Create by: LeoWu
+ * @Date: 2020/10/26
+ */
+public class UseInet {
+    private static final Logger log = LogManager.getLogger(UseInet.class);
+
+    public static void main(String[] args) {
+
+    }
+}
